2d0f85194734830bdcb9ad113de5349bffa659c4
slepov0 <olegmps2014@gmail.com>
2022-12-26 16:12:25 +0300

    added spring project

---
+++ src/main/java/ru/slepov0/testsite/TestsiteApplication.java
@@ -0,0 +1,13 @@
+package ru.slepov0.testsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/slepov0/testsite/TestsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.slepov0.testsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
